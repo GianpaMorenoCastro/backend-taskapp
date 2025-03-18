5ecae4e5e4ceccb347fc144566ada838b2c44663
Gianpablo Moreno Castro <169085793+GianpaMorenoCastro@users.noreply.github.com>
2025-03-17 21:04:19 -0600

    config de gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,22 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.1.2"
-	id("io.spring.dependency-management") version "1.1.2"
-	kotlin("jvm") version "1.8.22"
-	kotlin("plugin.spring") version "1.8.22"
+	id("org.springframework.boot") version "3.2.2"
+	id("io.spring.dependency-management") version "1.1.4"
+	kotlin("jvm") version "1.9.0"
+	kotlin("plugin.spring") version "1.9.0"
 	kotlin("plugin.jpa") version "1.9.0"
 	kotlin("kapt") version "1.9.0"
+    id("com.android.library") version "8.1.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+	id("com.android.application") version "8.1.1" apply false
 }
 
 group = "edu.backend"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_17
+	toolchain.languageVersion.set(JavaLanguageVersion.of(17))
 }
 repositories {
 	mavenCentral()
@@ -25,13 +28,11 @@ dependencies {
 	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
 	implementation ("org.mapstruct:mapstruct:1.5.5.Final")
 	kapt("org.mapstruct:mapstruct-processor:1.5.5.Final")
-	annotationProcessor ("org.mapstruct:mapstruct:1.5.2.Final")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	kapt("org.springframework.boot:spring-boot-configuration-processor")
 	runtimeOnly("org.postgresql:postgresql")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
@@ -48,4 +49,11 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+allprojects {
+	repositories {
+		google()
+		mavenCentral()
+	}
 }
